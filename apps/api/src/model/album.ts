6dac806fa57c3c61c986d96e664c60b35aaa60e4
Arthur Wang <codecwang@outlook.com>
2024-08-19 23:55:42 +0800

    add pinned field to album model

--- apps/api/src/model/album.ts
+++ apps/api/src/model/album.ts
@@ -14,6 +14,10 @@ export default (sequelize: Sequelize) => {
         allowNull: false,
         unique: true,
       },
+      pinned: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
       createdAt: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
