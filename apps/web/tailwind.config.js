42d8ed13979a0e79f1f091bb05ac5da7e3c8a701
arthur <codecwang42@gmail.com>
2025-03-25 21:04:39 +0800

    remove daisyui

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,18 +1,16 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
-const daisyui = require('daisyui');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(
       __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}',
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
 };
